adce98a2d0860f7cac8b3d5240acbbe8c0aad4b9
avenmerk <=>
2019-02-06 13:32:54 +0300

    clean up

--- src/components/toggler.js
+++ src/components/toggler.js
@@ -15,6 +15,7 @@ class Toggler extends Component {
         animationToLeftX2: "animate-it-to-left-2",
     };
 
+    //Calculating moving
     movePolygon = (e) => {
         e.preventDefault();
         console.log('movePolygon');
@@ -22,12 +23,15 @@ class Toggler extends Component {
         const polygon = document.getElementById('polygon-1');
         const line = document.getElementById('active-line');
 
+        // Add animation to right/left depends on previous position of polygon
+
         if (newPosition < this.state.posCurrent) {
             this.addAnimateToLeftClass(newPosition);
         } else if (newPosition > this.state.posCurrent) {
             this.addAnimateToRightClass(newPosition);
         }
 
+        //Restrict diapason of movements
         if (newPosition <= this.state.posMin) {
             this.setState({posCurrent: this.state.posMin});
             polygon.style.left = `${this.state.posMin}px`;
@@ -36,6 +40,8 @@ class Toggler extends Component {
             this.setState({posCurrent: this.state.posMax});
             polygon.style.left = `${this.state.posMax}px`;
             line.style.left = `0px`;
+
+            //Specify movements
         } else {
             this.setState({posCurrent: newPosition});
             this.setState({linePosCurrent: this.state.linePosStart + (newPosition - this.state.posMin)});
@@ -44,6 +50,7 @@ class Toggler extends Component {
         }
     };
 
+    //Add closer to get to min/mid/max point automatically
     polygonCloser = (e) => {
         e.preventDefault();
         console.log('polygonCloser');
@@ -53,7 +60,10 @@ class Toggler extends Component {
         const line = document.getElementById("active-line");
         const style = document.documentElement.style;
 
-//get to middle from left
+    // set parameters of animation, add animation className to polygon and line,
+        //then change their current position to state
+
+        //to get to middle from left
         if (pos >= (this.state.posMiddle - 160) && pos <= this.state.posMiddle) {
             style.setProperty('--lineCurrent', `${linePos}px`);
             style.setProperty('--lineCloser', `-320px`);
@@ -63,6 +73,7 @@ class Toggler extends Component {
             polygon.classList.add('closer-pos');
             line.classList.add('closer-line');
             this.setState( {posCurrent: this.state.posMiddle, linePosCurrent: -320});
+            this.addAnimateToRightClass(this.state.posMiddle);
 
             //get to max
         } else if (pos >= (this.state.posMax - 160)) {
@@ -74,6 +85,7 @@ class Toggler extends Component {
             polygon.classList.add('closer-pos');
             line.classList.add('closer-line');
             this.setState( {posCurrent: this.state.posMax, linePosCurrent: 0});
+            this.addAnimateToRightClass(this.state.posMax);
 
             //get to min
         } else if (pos < (this.state.posMin + 160)) {
@@ -85,6 +97,7 @@ class Toggler extends Component {
             polygon.classList.add('closer-pos');
             line.classList.add('closer-line');
             this.setState( {posCurrent: this.state.posMin, linePosCurrent: this.state.linePosStart});
+            this.addAnimateToLeftClass(this.state.posMin);
 
             //get to middle from right
         } else if (pos < (this.state.posMiddle + 160) && pos > this.state.posMiddle) {
@@ -96,7 +109,11 @@ class Toggler extends Component {
             polygon.classList.add('closer-pos');
             line.classList.add('closer-line');
             this.setState( {posCurrent: this.state.posMiddle, linePosCurrent: -320});
+            this.addAnimateToLeftClass(this.state.posMiddle);
         }
+
+        //add listener for animation end to set new position for elements and remove animation class
+        // (to be able add this animation again)
         polygon.addEventListener("webkitAnimationEnd",
             ( () => {
                 polygon.style.left = `${this.state.posCurrent}px`;
@@ -107,15 +124,16 @@ class Toggler extends Component {
             ));
     };
 
+    //add animation from left to right depends on current position of element
     addAnimateToRightClass = (position) => {
         const bgArray = document.getElementById('box3').children;
         for (let i = 0; i < bgArray.length; i++) {
-            if ( position <= this.state.posMiddle && position>= (this.state.posMiddle - 160)) {
+            if ( position <= this.state.posMiddle && position>= (this.state.posMiddle - 60)) {
                 bgArray[i].classList.remove(this.state.animationToLeft);
                 bgArray[i].classList.remove(this.state.animationToLeftX2);
                 bgArray[i].classList.add(this.state.animationToRight);
 
-            } else if (position <= this.state.posMax && position>= (this.state.posMax - 160)) {
+            } else if (position <= this.state.posMax && position>= (this.state.posMax - 60)) {
                 bgArray[i].classList.add(this.state.animationToRightX2);
                 bgArray[i].classList.remove(this.state.animationToLeft);
                 bgArray[i].classList.remove(this.state.animationToLeftX2);
@@ -123,14 +141,15 @@ class Toggler extends Component {
         }
     };
 
+    //add animation from right to left depends on current position of element
     addAnimateToLeftClass = (position) => {
         const bgArray = document.getElementById('box3').children;
         for (let i = 0; i < bgArray.length; i++) {
-            if (position >= this.state.posMin && position <= (this.state.posMin + 160)) {
+            if (position >= this.state.posMin && position <= (this.state.posMin + 60)) {
                 bgArray[i].classList.add(this.state.animationToLeftX2);
                 bgArray[i].classList.remove(this.state.animationToRight);
                 bgArray[i].classList.remove(this.state.animationToRightX2);
-            } else if (position >= this.state.posMiddle && position <= (this.state.posMiddle + 160)) {
+            } else if (position >= this.state.posMiddle && position <= (this.state.posMiddle + 60)) {
                 bgArray[i].classList.remove(this.state.animationToRightX2);
                 bgArray[i].classList.add(this.state.animationToLeft);
             }
